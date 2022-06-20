994e3978ddf33f0a5b1731299d6cd41ba9b73c92
FPS man <artemisbeams@gmail.com>
2022-06-20 15:50:40 -0700

    finsihed 4 today

--- real.py
+++ real.py
@@ -5,19 +5,30 @@ from math import *
 hub = PrimeHub()
 
 hub.light_matrix.show_image('HAPPY')
-
 corPlane = []
+# the bigger the number is the more accurate the code is gonna be 
 tot = [x: 6, y: 6]
-
 # make sure tot and scale factor work toghther
-startPoint = [1,1]
-scaleFactor = 1 in
+scaleFactor = 1
 
+hub.motion_sensor.reset_yaw_angle()
+motion.set_degrees_counted()
+# hub.motion_sensor.get_yaw_angle() is how to find 
 
-def makeMap():
+# start point should be a list: eg [1,3]
+def makeMap(startPoint):
     global corPlane, tot
     x = tot.x
     y = tot.y
-
+    # fill out the corPlane
     for xPlace in x:
-#0 is nothing there, 1 is where we are
+        corPlane.append([])
+        for yPlace in y:
+            corPlane[xPlace].append(0)
+    #0 is nothing there, 1 is where we are
+    corPlane[startPoint[0]][startPoint[1]] =1
+
+def turn(desiredAngle):
+    currentAng = hub.motion_sensor.get_yaw_angle()
+    change = desiredAngle - currentAng
+    return change
